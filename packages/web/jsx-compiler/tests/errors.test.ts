c5fae9e1cb6aaf7e87f2b31a30203f0eeb1b2499
Manan Tank <manantankm@gmail.com>
2022-05-03 19:01:28 +0530

    jsx-compiler: refactor errors.test

--- packages/web/jsx-compiler/tests/errors.test.ts
+++ packages/web/jsx-compiler/tests/errors.test.ts
@@ -1,26 +1,26 @@
 import { testPlugin } from './testPlugin'
 
-test('no errors', () => {
-  expect(() => testPlugin('<div />')).not.toThrow()
+describe('Does not throw error for', () => {
+  test('Known $: props', () => {
+    expect(() => testPlugin('<input $:selected={bar} />')).not.toThrow()
+    expect(() => testPlugin('<input $:value={bar} />')).not.toThrow()
+    expect(() => testPlugin('<input $:checked={bar} />')).not.toThrow()
+  })
 })
 
-test('fragments not supported', () => {
-  // fragments inside function scope
-  expect(() => testPlugin('const x = () => <> </>')).toThrow()
-  // fragments at program scope
-  expect(() => testPlugin('const x = <> </>')).toThrow()
-  // fragments at program scope deep in jsx
-  expect(() =>
-    testPlugin('<div> <div> <div> <> </> </div> </div> </div>')
-  ).toThrow()
-})
+describe('Throws error for', () => {
+  test('JSXFragmentElement', () => {
+    // fragments inside function scope
+    expect(() => testPlugin('const x = () => <> </>')).toThrow()
+    // fragments at program scope
+    expect(() => testPlugin('const x = <> </>')).toThrow()
+    // fragments at program scope deep in jsx
+    expect(() =>
+      testPlugin('<div> <div> <div> <> </> </div> </div> </div>')
+    ).toThrow()
+  })
 
-test('invalid special props', () => {
-  expect(() => testPlugin('<input $:foo={bar} />')).toThrow()
-})
-
-test('valid special props', () => {
-  expect(() => testPlugin('<input $:selected={bar} />')).not.toThrow()
-  expect(() => testPlugin('<input $:value={bar} />')).not.toThrow()
-  expect(() => testPlugin('<input $:checked={bar} />')).not.toThrow()
+  test('Unknown $: prop', () => {
+    expect(() => testPlugin('<input $:foo={bar} />')).toThrow()
+  })
 })
