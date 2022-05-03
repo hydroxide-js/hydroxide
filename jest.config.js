7f5e639eccd3be9d77dbbf1e2d678f2a74acbf86
Manan Tank <manantankm@gmail.com>
2022-05-03 12:01:18 +0530

    update all

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,16 @@
-// eslint-disable-next-line
-const path = require('path')
-
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'jsdom',
   transform: {
+    // use ts-jest for typescript files
     '^.+\\.(ts|tsx)?$': 'ts-jest',
+    // use babel-jest for javascript files
     '^.+\\.(js|jsx)$': 'babel-jest'
   },
+  // while testing point directly to .ts files instead of bundles
   moduleNameMapper: {
-    '@nuejs/core': path.join(__dirname, './packages/core/src/index'),
-    '@nuejs/web': path.join(__dirname, './packages/web/src/index')
+    '@nuejs/core': '<rootDir>/packages/core/src/index',
+    '@nuejs/web': '<rootDir>/packages/web/renderer/src/index'
   }
 }
