e1a5c5736cccc5c785aa31f5bcf91cbd3914984c
Manan Tank <manantankm@gmail.com>
2022-06-21 00:07:52 +0530

    ignore dist from jest coverage

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
     '<rootDir>/packages/core/jest.config.js',
     '<rootDir>/packages/web/renderer/jest.config.js',
     '<rootDir>/packages/web/jsx-compiler/jest.config.js'
-  ]
+  ],
+  collectCoverageFrom: ['**/src/**/*.ts', '!**/node_modules/**']
 }
