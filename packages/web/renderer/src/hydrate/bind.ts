ceb91b7465c0cd77b46b12366c7fde30800a69a1
Manan Tank <manantankm@gmail.com>
2022-07-18 00:07:32 +0530

    add bind API

---
+++ packages/web/renderer/src/hydrate/bind.ts
@@ -0,0 +1,36 @@
+import { ReactiveSlice, Reactive, GenericPath, effect, valueAt } from 'hydroxide'
+
+export function bind(
+  node: HTMLElement,
+  prop: string,
+  updator: Reactive<any> | ReactiveSlice<any>
+) {
+  let state: Reactive<any>
+  let path: GenericPath | undefined
+
+  if ('reactive' in updator) {
+    state = updator.reactive
+    path = updator.path
+  } else {
+    state = updator
+  }
+
+  const initValue = path ? valueAt(state.value, path) : state.value
+  const type = typeof initValue
+
+  // @ts-expect-error
+  node.$$input = (event: InputEvent) => {
+    // @ts-ignore
+    let value = event.target[prop]
+    if (type === 'number') {
+      value = Number(value)
+    }
+
+    updator.set(value)
+  }
+
+  effect(() => {
+    // @ts-expect-error
+    node[prop] = path ? valueAt(state(), path) : state()
+  })
+}
