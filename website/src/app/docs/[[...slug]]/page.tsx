3da2be6259aea3c6da909c07e5e2bc5ac749df93
Manan Tank <manantankm@gmail.com>
2026-02-05 02:36:04 +0530

    Update OG styles, Fix Open in Github link

--- website/src/app/docs/[[...slug]]/page.tsx
+++ website/src/app/docs/[[...slug]]/page.tsx
@@ -34,7 +34,7 @@ export default async function Page(props: PageProps<'/docs/[[...slug]]'>) {
         <ViewOptions
           markdownUrl={`${page.url}.mdx`}
           // update it to match your repo
-          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/docs/content/docs/${page.path}`}
+          githubUrl={`https://github.com/${gitConfig.user}/${gitConfig.repo}/blob/${gitConfig.branch}/website/content/docs/${page.path}`}
         />
       </div>
       <DocsBody>
